c28cc22cf4e228fa3592f8b6e0cba5a6c2c37f33
lukkelele <encryptedluke@protonmail.com>
2022-02-28 20:33:02 +0100

    update

--- school/1DV503/a2/sql_statements.py
+++ school/1DV503/a2/sql_statements.py
@@ -5,6 +5,10 @@ DB_NAME = "GUNNARSSON"
 end_statement = "\n);"
 
 
+def create_database(db_name):
+    query = f"CREATE DATABASE {db_name}"
+    return query
+
 
 def create_table(table_name, attributes):
     len_attributes = len(attributes)
@@ -12,18 +16,30 @@ def create_table(table_name, attributes):
     for attribute in attributes:
         name = attribute[0]
         attr_type = attribute[1]
+        constraint = ""
+        if len(attribute) > 2:
+            constraint = attribute[2]
+        
         if attributes.index(attribute) == (len_attributes - 1):     # if last attribute, dont add a ','
-            query += name + " " + attr_type + end_statement
+            #query += name + " " + attr_type + " " + constraint + end_statement
+            query += f"{name} {attr_type} {constraint} {end_statement}"
             break
-        query += name + " " + attr_type + ",\n"
+        else:
+            query += f"{name} {attr_type} {constraint},\n"
     return query 
-
 
 
 # s_attr ==> string attribute instead of a list
 def alter_table(table_name, method, s_attr):
-    query = f"ALTER TABLE {table_name}\n{method} {s_attr}"
-    return query
+    approved_methods = ["ADD", "DROP"]
+    for approved_method in approved_methods:
+        if method == approved_method:
+            query = f"ALTER TABLE {table_name}\n{method} {s_attr} {end_statement}"
+            return query
+        else:
+            return ""
+
+
 
 
 
@@ -31,6 +47,7 @@ def alter_table(table_name, method, s_attr):
 
 # Tests
 
-attributes = [["PersonID", "int"], ["FirstName", "varChar(255)"], ["LastName", "varchar(255)"]]
+attributes = [["PersonID", "int"], ["FirstName", "varChar(255)", "NOT NULL"], ["LastName", "varchar(255)"]]
 
 s = create_table("Person", attributes)
+print(s)
