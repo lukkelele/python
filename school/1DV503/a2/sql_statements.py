a6a85d696109ce88570d7533118124f263d74fb3
lukkelele <encryptedluke@protonmail.com>
2022-02-28 19:35:47 +0100

    update

--- school/1DV503/a2/sql_statements.py
+++ school/1DV503/a2/sql_statements.py
@@ -2,28 +2,40 @@ import mysql.connector
 
 
 DB_NAME = "GUNNARSSON"
+end_statement = "\n);"
+
+# Attributes is a list holding a list[2] with attribute name and type
+def create_table(table_name, attributes):
+    len_attributes = len(attributes)
+    attr_string = ""
+    query = f"CREATE TABLE {table_name} ("
+    for attribute in attributes:
+        if attributes.index(attribute) == (len_attributes - 1):     # if last attribute, dont add a ','
+            query =+ name + " " + attr_type + end_statement
+        name = attribute[0]
+        attr_type = attribute[1]
+        query =+ name + " " + attr_type + ","
+        
 
 
-def create_table(attributes):
-    attributes.
 
-    return
+cursor.execute()
 
 
 
 
+try:
+    db = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="root",
+            database=DB_NAME
+            )
+except:     # if no database is recognized by DB_NAME
+    db = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="root"
+            )
 
-
-#try:
-#    db = mysql.connector.connect(
-#            host="127.0.0.1",
-#            user="root",
-#            passwd="root",
-#            database=DB_NAME
-#            )
-#except:     # if no database is recognized by DB_NAME
-#    db = mysql.connector.connect(
-#            host="127.0.0.1",
-#            user="root",
-#            passwd="root",
-#            )
+cursor = db.cursor()
