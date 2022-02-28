dbb230f862670388697afad1f38b63acbb192268
unknown <encryptedluke@protonmail.com>
2022-02-28 18:04:40 +0100

    new files

--- school/1DV503/a2/admin.py
+++ school/1DV503/a2/admin.py
@@ -1,27 +1,59 @@
 import mysql.connector
 import csv
 
-db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="root"
-        #database=""
-        )
 
-DB_name = "GUNNARSSON"      # last name
+DB_name = "GUNNARSSON"
 
-cursor = db.cursor()
+# Try to connect to a database that is named GUNNARSSON
 
-
-
-
+def print_line(n):
+    c = 0
+    while c < n:
+        print("=", end="")
+        c += 1
 
 
 def main_menu():
-    print
+    print("\n")
+    print_line(19)
+    print(" MAIN MENU ", end="")
+    print_line(19)
+    print("\n")
+    print("(1) LIST ALL PLANETS\n(2) SEARCH FOR PLANET DETAILS\n(3)"+
+            " SEARCH FOR SPECIES ABOVE A CERTAIN HEIGHT\n(4) GUESS THE DESIRED CLIMATE\n(5) SHOW AVERAGE LIFESPAN PER SPECIES CLASSIFICATION\n")
+    print_line(50)
 
 
 def print_rows(csv_file):
     file = open(csv_file,'r')
     for row in file:
       print(row)
+
+
+
+try:
+    db = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="root",
+            database=DB_name
+            )
+except:
+    print(f"No database found going by name {DB_name}.\nConnecting without a specified database instead.")
+    db = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="root"
+            )
+
+
+cursor = db.cursor()    # Create cursor object
+
+main_menu()
+
+
+
+
+
+
+
---
+++ school/1DV503/a2/sql_statements.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+
+DB_NAME = "GUNNARSSON"
+
+
+def create_table(attributes):
+    attributes.
+
+    return
+
+
+
+
+
+
+#try:
+#    db = mysql.connector.connect(
+#            host="127.0.0.1",
+#            user="root",
+#            passwd="root",
+#            database=DB_NAME
+#            )
+#except:     # if no database is recognized by DB_NAME
+#    db = mysql.connector.connect(
+#            host="127.0.0.1",
+#            user="root",
+#            passwd="root",
+#            )
