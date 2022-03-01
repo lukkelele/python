73533a7ace4f5aaa72a6f3ed697295c70afab07e
lukkelele <encryptedluke@protonmail.com>
2022-03-01 13:48:51 +0100

    upd

--- school/1DV503/a2/sql_statements.py
+++ school/1DV503/a2/sql_statements.py
@@ -10,36 +10,47 @@ def create_database(db_name):
     return query
 
 
-def create_table(table_name, attributes):
+# [NAME, DATATYPE, CONSTRAINT, KEY PROPERTY]
+def create_table(table, attributes):
     len_attributes = len(attributes)
-    query = f"CREATE TABLE {table_name} (\n"
+    query = f"CREATE TABLE {table} (\n"
     for attribute in attributes:
         name = attribute[0]
         attr_type = attribute[1]
         constraint = ""
+        key_property = ""
         if len(attribute) > 2:
             constraint = attribute[2]
-        
+            if len(attribute) > 3:                  # if primary key notation as an example
+                key_property = attribute[3]
         if attributes.index(attribute) == (len_attributes - 1):     # if last attribute, dont add a ','
             #query += name + " " + attr_type + " " + constraint + end_statement
-            query += f"{name} {attr_type} {constraint} {end_statement}"
+            query += f"{name} {attr_type} {constraint} {key_property} {end_statement}"
             break
         else:
-            query += f"{name} {attr_type} {constraint},\n"
+            query += f"{name} {attr_type} {constraint} {key_property},\n"
     return query 
 
 
 # s_attr ==> string attribute instead of a list
-def alter_table(table_name, method, s_attr):
+def alter_table(table, method, s_attr):
     approved_methods = ["ADD", "DROP"]
     for approved_method in approved_methods:
         if method == approved_method:
-            query = f"ALTER TABLE {table_name}\n{method} {s_attr} {end_statement}"
+            query = f"ALTER TABLE {table}\n{method} {s_attr} {end_statement}"
             return query
         else:
             return ""
 
 
+def list_planets(table):
+    query = f"""
+    
+    """
+
+
+def commit():
+    query = f""
 
 
 
