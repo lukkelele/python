84bb52d82d5db87ab6643e652d65dd3726d252e2
unknown <encryptedluke@protonmail.com>
2022-03-03 12:21:40 +0100

    updated

--- school/1DV503/a2/admin.py
+++ school/1DV503/a2/admin.py
@@ -26,16 +26,22 @@ DB_name = "gunnarss2on"
 # Planet, Specie, Environment, Color
 
 
-planet_datatypes = [["p_name", "varchar(20)", "NOT NULL", "PRIMARY KEY"], ["rotation_period", "int"], ["orbital_period", "int"]
-                   ,["diameter","long"], ["climate", "varchar(20)"], ["gravity", "decimal(2,2)"], 
+planet_datatypes = [["p_name", "varchar(20)", "NOT NULL", "PRIMARY KEY"], ["rotation_period", "int"], 
+                    ["orbital_period", "int"], ["diameter","long"], ["climate", "varchar(20)"],
+                    ["gravity", "decimal(2,2)"], 
                     ["terrain", "varchar(20)"], ["surface_water", "int"], ["population", "bigint"]]
 
 specie_datatypes = [["s_name", "varchar(15)", "NOT NULL", "PRIMARY KEY"], ["classification", "varchar(15)"],
-                    ["designation", "varchar(14)"] ,["average_height", "int"], ["skin_colors", "varchar(14)"],
-                    ["hair_colors", "varchar(12)"], ["eye_colors", "varchar(10)"], ["average_lifespan", "int"],
-                    ["language", "varchar(18)"], ["homeworld", "varchar(14)"]]
+                    ["designation", "varchar(14)"] ,["average_height", "int"], 
+                    ["average_lifespan", "int"], ["language", "varchar(18)"], ["homeworld", "varchar(14)"]]
 
 environment_datatypes = [["p_name", "varchar(14)", "NOT NULL", "PRIMARY KEY"], ["terrain", "varchar(12)"]]
+
+terrain_datatypes = [["p_name", "varchar(14)", "NOT NULL", "PRIMARY KEY"], ["terrain", "varchar(12)"]]      # FIX
+
+hair_color_datatypes = [["s_name", "varchar(20)"], ["hair_color", "varchar(14)", "PRIMARY KEY hair_color_PK (hair_color)"]]
+eye_color_datatypes =  [["s_name", "varchar(20)"], ["eye_color",  "varchar(14)", "PRIMARY KEY eye_color_PK  (eye_color)" ]]
+skin_color_datatypes = [["s_name", "varchar(20)"], ["skin_color", "varchar(14)", "PRIMARY KEY skin_color_PK (hair_color)"]]
 
 
 def user_input():
@@ -54,6 +60,13 @@ def parse_csv_file(path, cursor):
         for row in file_reader:
             cursor.execute(query, row)
         cursor.commit()
+
+
+
+def add_FOREIGN_KEY(cursor, table, attr):
+    query = f"ALTER TABLE {table} ADD FOREIGN KEY {attr}_FK ({attr});"
+    cursor.execute(query)
+    print(f"New foreign key on table {table} added onto {attr}!")
 
 
 def read_multivalued_attribute(path, table, attr):
@@ -83,6 +96,32 @@ def get_tables(cursor):
     for table in cursor:
         print(table)
 
+
+def new_database():
+    print(f"No database found going by name {DB_name}.\nConnecting without a specified database instead.")
+    db = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        passwd="root"
+        )
+    cursor = db.cursor()
+    print(f"Creating new database named {DB_name}.")
+    cursor.execute("CREATE DATABASE {};".format(DB_name))
+    cursor.execute("USE {}".format(DB_name))                # Set new database as default
+    # Create all tables
+    cursor.execute(SQL.create_table("Specie", specie_datatypes))    # Specie Entity
+    cursor.execute(SQL.create_table("Planet", planet_datatypes))
+    cursor.execute(SQL.create_table("Environment", environment_datatypes))
+    cursor.execute(SQL.create_table("Hair_Color", environment_datatypes))
+    cursor.execute(SQL.create_table("Eye_Color",  eye_color_datatypes))
+    cursor.execute(SQL.create_table("Skin_Color", skin_color_datatypes))
+    
+    cursor.execute("ALTER TABLE Hair_Color ADD FOREIGN KEY (p_name) REFERENCES Specie(p_name) ON DELETE CASCADE;")
+    cursor.execute("ALTER TABLE Eye_Color  ADD FOREIGN KEY (p_name) REFERENCES Specie(p_name) ON DELETE CASCADE;")
+    cursor.execute("ALTER TABLE Skin_Color ADD FOREIGN KEY (p_name) REFERENCES Specie(p_name) ON DELETE CASCADE;")
+    
+    print("Getting tables")
+    get_tables(cursor)
 
 
 
@@ -117,6 +156,17 @@ if flag == False:
     cursor.execute("CREATE DATABASE {};".format(DB_name))
     cursor.execute("USE {}".format(DB_name))
     # Create all tables
+    
+    cursor.execute("""CREATE TABLE Specie (
+	s_name varchar(20) PRIMARY KEY,
+    classification varchar(18),
+	designation    varchar(18),
+    average_height   int,
+    average_lifespan int,
+    s_language     varchar(18),
+    homeworld      varchar(18)
+    );""")
+
     cursor.execute(SQL.create_table("Planet", planet_datatypes))
     cursor.execute(SQL.create_table("Specie", specie_datatypes))
     cursor.execute(SQL.create_table("Environment", environment_datatypes))
@@ -124,7 +174,7 @@ if flag == False:
     print("Getting tables")
     get_tables(cursor)
     print("Parsing CSV file")
-    parse_csv_file("./data/planets.csv", cursor)
+    #parse_csv_file("./data/planets.csv", cursor)
 
 
 else:
