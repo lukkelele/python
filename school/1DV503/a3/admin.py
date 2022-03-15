787d60c496f3d31860d429634752974150e4d80a
lukkelele <encryptedluke@protonmail.com>
2022-03-15 11:07:12 +0100

    Further update

--- school/1DV503/a3/admin.py
+++ school/1DV503/a3/admin.py
@@ -38,21 +38,27 @@ class Admin:
         #self.db.autocommit()
         #print("Autocommit set to TRUE")
         self.cursor = self.db.cursor()
-        self.cursor.execute("use library_db;")
 
     def check_database_existance(self):
         if self.connection_flag == False:
+            self.cursor.execute("create database library_db;")
+            self.cursor.execute("use library_db;")
+            self.db.commit()
             print("Creating tables..")
             # Create all tables if no database
-            self.cursor.execute(query.create_table("Librarian", query.get_attributes("Librarian")))
-
+         #   self.cursor.execute(query.create_table("Librarian", query.get_attributes("Librarian")))
+        else:
+            print("Database exists!")
+            # Debug purposes
 
 if __name__ == "__main__":
     admin = Admin("root", "root", "localhost", "library_db")
     admin.start()
+    admin.connection_flag = False
     admin.check_database_existance()
     admin.cursor.execute(query.create_table("Librarian", query.get_attributes("Librarian")))
     admin.cursor.execute("insert into Librarian values (1, \"Lukas\", \"Gunnarsson\");")
+    admin.cursor.execute(query.insert_to_table("Librarian", "1, \"Migge\", \"Holm\""))
     admin.db.commit()
     print("Exiting...")
 
--- school/1DV503/a3/sql_handler.py
+++ school/1DV503/a3/sql_handler.py
@@ -4,6 +4,12 @@ librarian_attr = "emp_id int, f_name varchar(12), l_name varchar(20)"
 book_attr = "isbn int, title varchar(16), genre varchar(12), price int, publication int"
 
 
+def get_attributes(entity):
+    entity = entity.lower() 
+    if entity == "librarian": return librarian_attr
+    if entity == "book":      return book_attr
+
+
 def create_table(name, attributes):
     query = f"""
     CREATE TABLE {name} (
@@ -13,7 +19,6 @@ def create_table(name, attributes):
     return query
 
 
-def get_attributes(entity):
-    entity = entity.lower() 
-    if entity == "librarian": return librarian_attr
-    if entity == "book":      return book_attr
+def insert_to_table(name, value):
+    query = f"INSERT INTO {name} VALUES({value});"
+    return query
