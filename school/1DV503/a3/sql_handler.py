acca4dcd9214d17664243832c68fb1363eb37364
lukkelele <encryptedluke@protonmail.com>
2022-03-15 19:56:05 +0100

    update

--- school/1DV503/a3/sql_handler.py
+++ school/1DV503/a3/sql_handler.py
@@ -1,7 +1,48 @@
 # SQL queries
 
-librarian_attr = "emp_id int, f_name varchar(12), l_name varchar(20)"
-book_attr = "isbn int, title varchar(16), genre varchar(12), price int, publication int"
+user_attr = """user_id int,
+                f_name varchar(12),
+                l_name varchar(20),
+                gender varchar(10),
+                phone int,
+                address varchar(20),
+                PRIMARY KEY (user_id)"""
+
+librarian_attr = """emp_id int,
+                    f_name varchar(12),
+                    l_name varchar(20),
+                    gender varchar(10),
+                    phone int,
+                    address varchar(20),
+                    PRIMARY KEY (emp_id)"""
+
+book_attr = """isbn int, 
+               title varchar(16),
+               genre varchar(12),
+               price int,
+               publication int, 
+               PRIMARY KEY (isbn)"""
+
+library_attr = """lib_id int,
+                  lib_name varchar(18),
+                  address varchar(20),
+                  city varchar(16),
+                  zipcode int,
+                  company varchar(20),
+                  PRIMARY KEY (lib_id)"""
+
+author_attr = """author_id int,
+                 f_name varchar(12),
+                 l_name varchar(20),
+                 PRIMARY KEY (author_id)"""
+
+# Fix references..
+has_published_attr = """author_id int,
+                        isbn int,
+                        publisher varchar(20),
+                        PRIMARY KEY (author_id, isbn),
+                        FOREIGN KEY author_key (author_id) REFERENCES Author(author_id),
+                        FOREIGN KEY isbn_key (isbn) REFERENCES Book(isbn)"""
 
 
 def get_attributes(entity):
