06c7cc8f9ca1bbb2c4cecedbf04e3e6f662d5f83
lukkelele <encryptedluke@protonmail.com>
2022-03-15 09:43:06 +0100

    Progres..

--- school/1DV503/a3/admin.py
+++ school/1DV503/a3/admin.py
@@ -1,5 +1,5 @@
 import mysql.connector as mysql
-
+import sql_handler as query
 
 class Admin:
 
@@ -8,6 +8,8 @@ class Admin:
         self.passwd = passwd
         self.url = url
         self.db_name = db_name
+        self.connection_flag = False
+
 
     def connect_database(self):
         try:
@@ -28,7 +30,29 @@ class Admin:
                     passwd=self.passwd
                     )
             print(f"Connected without database specified.\nCredentials:\nuser: {self.user}\nurl: {self.url}")
-            self.connection_flag = False
         return db
+    
 
+    def start(self):
+        self.db = self.connect_database()
+        #self.db.autocommit()
+        #print("Autocommit set to TRUE")
+        self.cursor = self.db.cursor()
+        self.cursor.execute("use library_db;")
+
+    def check_database_existance(self):
+        if self.connection_flag == False:
+            print("Creating tables..")
+            # Create all tables if no database
+            self.cursor.execute(query.create_table("Librarian", query.get_attributes("Librarian")))
+
+
+if __name__ == "__main__":
+    admin = Admin("root", "root", "localhost", "library_db")
+    admin.start()
+    admin.check_database_existance()
+    admin.cursor.execute(query.create_table("Librarian", query.get_attributes("Librarian")))
+    admin.cursor.execute("insert into Librarian values (1, \"Lukas\", \"Gunnarsson\");")
+    admin.db.commit()
+    print("Exiting...")
 
--- school/1DV503/a3/sql_handler.py
+++ school/1DV503/a3/sql_handler.py
@@ -1,5 +1,7 @@
 # SQL queries
 
+librarian_attr = "emp_id int, f_name varchar(12), l_name varchar(20)"
+book_attr = "isbn int, title varchar(16), genre varchar(12), price int, publication int"
 
 
 def create_table(name, attributes):
@@ -9,3 +11,9 @@ def create_table(name, attributes):
     );
     """
     return query
+
+
+def get_attributes(entity):
+    entity = entity.lower() 
+    if entity == "librarian": return librarian_attr
+    if entity == "book":      return book_attr
