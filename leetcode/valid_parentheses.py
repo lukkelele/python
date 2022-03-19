b4da55ccd3f15d16e3b966ff297073acff90d347
lukkelele <encryptedluke@protonmail.com>
2022-03-19 11:04:55 +0100

    Update

--- leetcode/valid_parentheses.py
+++ leetcode/valid_parentheses.py
@@ -9,42 +9,14 @@ class Solution:
         half = (len_s/2) - 1
         end = len_s - 1
         index = 0
-        first_char = s[index]
-        second_char = s[index+1]
-        chars = first_char + second_char
-        if len_s == 2:
-            if s == "()" or s == "[]" or s == "{}":
-                return True
-            else: return False
-        elif chars == "()" or chars == "[]" or chars == "{}":
-            while index < end:
-                print(f"len_s == 2  |  s[index]: {s[index]}\ns[index+1]: {s[index+1]}")
-                first_char = s[index]
-                second_char = s[index+1]
-                chars = first_char + second_char
-                print(f"chars ==> {chars}")
-                if chars == "()" or chars == "[]" or chars == "{}":
-                    index += 2
-                    continue
-                return False
-            return True
-        else:
-            print("entered lower")
-            while index <= half:
-                print(f"s[index]: {s[index]}\ns[end-index]: {s[end-index]}")
-                print(f"half=={half}")
-                char1 = s[index]
-                char2 = s[end-index]
-                chars = char1 + char2
-                if chars == "()" or chars == "[]" or chars == "{}":
-                    index += 1
-                    continue
-                return False
-                print(index)
-            return True
-
-
-
+        c = 0
+        char1 = s[index]
+        char2 = s[index+1]
+        mirror = [["(", ")"], ["[", "]"], ["{", "}"]]
+        if char1 == ")" or char1 == "]" or char1 == "}":
+            return False
+        while index < end:
+            print(f"current index: {index}")
 
 
 sol = Solution()
