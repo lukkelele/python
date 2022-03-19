ad3731ce8855acbf3899e654bebd14b25eb55e06
lukkelele <encryptedluke@protonmail.com>
2022-03-19 15:18:07 +0100

    update

--- leetcode/valid_parentheses.py
+++ leetcode/valid_parentheses.py
@@ -3,24 +3,47 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
+        closing = [")", "]", "}"]
+        brackets = ["(", "[", "{"]
         len_s = len(s)
         if len_s % 2 != 0:
             return False
         half = (len_s/2) - 1
         end = len_s - 1
         index = 0
-        c = 0
-        char1 = s[index]
-        char2 = s[index+1]
-        mirror = [["(", ")"], ["[", "]"], ["{", "}"]]
-        if char1 == ")" or char1 == "]" or char1 == "}":
+        try:
+            # if no error is raised the opening character is a closing bracket
+            closing.index(s[index])
             return False
-        while index < end:
-            print(f"current index: {index}")
+        except:
+            # IndexError -> not a closing bracket
+            print("First character was NOT a closing bracket.")
+            mirror = ""
+            sub_string = ""
+            res = ""
+            for character in s:
+                print(f"char: {character}")
+                if character == "(": mirror += ")"
+                elif character == "[": mirror += "]"
+                elif character == "{": mirror += "}"
+                for bracket in closing:
+                    if character == bracket:
+                        print(f"Closing bracket detected.\ns.index(character) = {s.index(character)}")
+                        sub_string = s[s.index(character):len(mirror)+s.index(character)]
+                        print(f"sub_string ==> {sub_string}\nmirror ==> {mirror}")
+                        if mirror == sub_string:
+                            return True
+                        else: return False
+
+
+
+
+
+
+
 
 
 sol = Solution()
-
 
 s1 = "()[]{}"
 s2 = "{[]}"
