d9a3c4962fd3aedbae9b11b7b18a5495aeb7c664
lukkelele <encryptedluke@protonmail.com>
2022-04-04 09:59:38 +0200

    little update

--- school/2DV516/a1/knn.py
+++ school/2DV516/a1/knn.py
@@ -3,16 +3,13 @@ import math
 import csv
 import numpy as np
 
-
-
 # Calculate distance for x
 # Sort the distance starting with the lowest values
 # Select k rows 
 # Calculate the mean
 
-
 # PLOT THE ORIGINAL DATA
-x_values = []
+values = []
 y_values = []
 
 chip1 = [-0.3, 1]
@@ -20,18 +17,18 @@ chip2 = [-0.5, -0.1]
 chip3 = [0.6, 0]
 
 
-
 # Calc distance between point z and ALL other points in the data set
 def calc_euclidean_distance(z):
     z0 = z[0]
     z1 = z[1]
     distances = []
-    for row in x_values:        # row[0] and row[1] --> x0 , x1
+    for row in values:        # row[0] and row[1] --> x0 , x1
         d = math.pow((z0 - float(row[1])), 2) + math.pow((z1 - float(row[1])), 2)
         distances.append([d, row[0], row[1], row[2]])
     # All distances calculated
     distances.sort()
     return distances
+
 
 # z is the point, k is the number of neighbors
 def find_neighbors(z, k):
@@ -41,10 +38,12 @@ def find_neighbors(z, k):
     while i < k:
         neighbors.append(d[i])
         i += 1
-    print("Neighbors found!")
+    print("\nNeighbors found!\n")
     for n in neighbors:
         print(n)
+    print()
     return neighbors
+
 
 
 def get_point_y(z, k):
@@ -56,10 +55,8 @@ def get_point_y(z, k):
 with open('./A1_datasets/microchips.csv') as csv_data:
     r = csv.reader(csv_data)
     for row in r:
-        x0_val = row[0]
-        x1_val = row[1]
-        y_val = row[2]
-        x_values.append([x0_val, x1_val, y_val]) 
+        # row[0] == x0_val  | row[1] == x1_val  | row[2] == y_val
+        values.append([row[0], row[1], row[2]]) 
 
 
 find_neighbors(chip1, 3)
