a0272fd51fef74f0201e970e0073274c2a11ca0f
lukkelele <encryptedluke@protonmail.com>
2022-04-06 20:07:17 +0200

    update

--- school/2DV516/a1/knn.py
+++ school/2DV516/a1/knn.py
@@ -20,7 +20,8 @@ simulation_k = [1, 3, 5, 7]  # k's to be used when simulating
 chip1 = [-0.3, 1]
 chip2 = [-0.5, -0.1]
 chip3 = [0.6, 0]
-chips = [chip1, chip2, chip3]
+chips = []
+test_chips = [chip1, chip2, chip3]
 chips_result = []
 
 
@@ -31,7 +32,7 @@ def open_csv_file(path):
             r = csv.reader(csv_data)
             for row in r:
                 # row[0] == x0_val  | row[1] == x1_val  | row[2] == y_val
-                print(row)
+            #    print(row)
                 values.append([row[0], row[1], row[2]]) 
     except: print("An error has occured!")
 
@@ -156,19 +157,27 @@ def simulate(k):
             flag = False
     if flag == True:
         draw_boundary()
+        base_plot()
         index_count = 0
         for i in k: # iterate list k
             print(f"\n-----------------\n| RUNNING k = {i} |\n-----------------")
             index_count += 1
-            base_plot()
             for chip in chips:
                 result = find_neighbors(chip, i)
                 if result == 0: p.scatter(float(chip[0]), float(chip[1]), color="r")
                 elif result == 1: p.scatter(float(chip[0]), float(chip[1]), color="g")
+            for chip in test_chips:
+                s = find_neighbors(chip, i)
+                print(f"s == {s}")
+                print_result(chip, i, s)
             for chip in chips_result:
 #                print(f"chip_result -> {chip}  |  s == {chip[1]}")
-                if int(chip[1]) == 0: p.scatter(float(chip[0][0]), float(chip[0][1]), color="b")
-                elif int(chip[1]) == 1: p.scatter(float(chip[0][0]), float(chip[0][1]), color="c")
+                if int(chip[1]) == 0:
+                    p.scatter(float(chip[0][0]), float(chip[0][1]), color="b", marker='X')
+                    print("0")
+                elif int(chip[1]) == 1:
+                    p.scatter(float(chip[0][0]), float(chip[0][1]), color="c", marker='X')
+                    print("1")
             p.subplot(2, 2, index_count)
             p.title(f"k == {i}")
 
