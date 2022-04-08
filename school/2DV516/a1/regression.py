2f8600dea289d93e6fda754eaecf5d2e35e7800a
lukkelele <encryptedluke@protonmail.com>
2022-04-08 20:00:59 +0200

    update

--- school/2DV516/a1/regression.py
+++ school/2DV516/a1/regression.py
@@ -58,6 +58,14 @@ def plot_data(data_set, c):
         y = float(point[1])
         p.scatter(x, y, color=c, s=6)
 
+def plot_boundary(k, data_set):
+    x_points = get_x_points()
+    y_vals = []
+    for x in x_points:
+        y = math_function(x)
+        y_vals.append(y)    
+    p.plot(x_points, y_vals, color="r")
+
 def calc_euclidean_distance(z):
     z0 = float(z[0])
     z1 = float(z[1])
@@ -71,21 +79,6 @@ def calc_euclidean_distance(z):
     dist = np.sort(dist, axis=0)
     return dist
 
-
-def get_near_values(x, k, data_set):
-    y_sum = 0
-    counter = 0
-    index = 0
-    index = np.searchsorted(data_set[:, 0], x, side="left")
-    while counter < k:
-        y_sum += data_set[index][1]
-        counter += 1
-        index += 1
-    average = y_sum/k
-    print(f"average ==> {average}")
-    return average
-    
-
 def get_neighbors(z, k, data_set):
     distances = calc_euclidean_distance(z)
     neighbors = []
@@ -95,62 +88,10 @@ def get_neighbors(z, k, data_set):
         counter += 1
     return neighbors
 
-def plot_boundary(k, data_set):
-    x_points = get_x_points(1)
-    y_vals = []
-    for x in x_points:
-        y = math_function(x)
-        y_vals.append(y)    
-    p.plot(x_points, y_vals, color="r")
-
-
-def calculate_y(x, data_set, k):
-    x_vals = find_close_x_values(x, data_set, k)
-    y_sum = 0
-    len_x_vals = len(x_vals)
-    for x_val in x_vals:
-        if x_val[0] != x:
-            y_sum += x_val[1]
-            #print(f"y_sum: {y_sum}\nx_vals = {x_vals[1]}")
-    if len_x_vals == 0: len_x_vals = 1 
-    average_y = float(y_sum / len_x_vals)
-    #print(f"average_y: {average_y}")
-    return average_y
-
-def find_close_x_values(x, data_set, k):
-    data_set = np.sort(data_set, axis=0)
-    index_reduction = math.floor(k/2)
-    x_index = 0
-    x_vals = []
-    for val in data_set:
-        x_vals.append(val)
-    #X = np.abs(data_set - x)
-    idx = (np.abs(data_set-x)).argmin()
-    #idx = np.where(X == X.min())
-    x_index = idx
-    print(x_index)
-    start = x_index - index_reduction
-    stop = x_index + index_reduction + 1
-    while stop > len(data_set):
-        stop -= 1
-    x_vals = x_vals[start:stop]
-    return x_vals
 
 # Return an array with a amount of equidistant x points
-def get_x_points(a):
-    np_maximum = np.max(data, axis=0) 
-    np_minimum = np.min(data, axis=0)
-    print(f"maximum val ==> {np_maximum}")
-    print(f"minimum val ==> {np_minimum}")
-    maximum = 0
-    minimum = 100
-    for val in np_maximum:
-        if val > maximum:
-            maximum = val
-    for val in np_minimum:
-        if val < minimum:
-            minimum = val
-    x_points = np.arange(1, 25, 0.1)
+def get_x_points():
+    x_points = np.arange(1, 25, 0.2)
     return x_points
 
 
@@ -160,6 +101,6 @@ training_set = all_data[1]
 
 
 plot_data(data, "b")
-plot_data(training_set, "k")
+#plot_data(training_set, "k")
 plot_boundary(3, data)
 p.show()
