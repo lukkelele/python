32782fcb38b11d14f58ee9b03e823d1d14f87700
lukkelele <encryptedluke@protonmail.com>
2022-04-11 08:44:27 +0200

    update on scatter point distribution

--- school/2DV516/a1/scikit.py
+++ school/2DV516/a1/scikit.py
@@ -5,19 +5,14 @@ import csv
 import math
 
 
-
-
-
-csv_path = "./A1_datasets/microchips.csv"
+path = "./A1_datasets/microchips.csv"
 
 simulation_k = [1, 3, 5, 7]  # k's to be used when simulating
 
 chip1 = [-0.3, 1]
 chip2 = [-0.5, -0.1]
 chip3 = [0.6, 0]
-chips = []
 test_chips = [chip1, chip2, chip3]
-chips_result = []
 
 # Open a csv file and read the data in to the list 'values'
 def open_csv_file(path):
@@ -34,29 +29,51 @@ def open_csv_file(path):
             return [np.array(X, dtype=float), np.array(y, dtype=float)]
     except: print("An error has occured!")
 
-data = open_csv_file(csv_path)
-X = data[0]
-x0 = data[0][:, 0]
-x1 = data[0][:, 1]
-y = data[1]
-test_x0 = np.arange(0, 118, 1)
-test_x1 = np.arange(0, 118, 1)
-test_set = np.matrix([test_x0, test_x1])
-test_set = np.reshape(test_set, (118,2))
+def load_data(path):
+    data = open_csv_file(path)
+    x0 = data[0][:, 0]
+    x1 = data[0][:, 1]
+    y = data[1]
+    test_x0 = np.arange(0, 118, 1)
+    test_x1 = np.arange(0, 118, 1)
+    test_set = np.matrix([test_x0, test_x1])
+    test_set = np.reshape(test_set, (118,2))
+    print(f"len_y: {len(y)}\nlen_x0: {len(x0)}\nlen_x1: {len(x1)}")
+    return test_set
 
-print(f"len_y: {len(y)}\nlen_x0: {len(x0)}\nlen_x1: {len(x1)}\nlen_X: {len(X)}")
+def plot_data(data):
+    x0 = data[0][:, 0]
+    x1 = data[0][:, 1]
+    Y = data[1]
+    X = np.array([x0, x1])
+    print(f"len_X: {len(X)}")
+    idx = 0
+    for x0 in X[0]:
+        y = Y[idx]
+        if y == 0:
+            point_color = "r"
+        else: point_color = "g"
+        x1 = X[1][idx]
+        plt.scatter(x0,x1, color=point_color)
+        idx += 1
+    plt.show()
 
-k = 3
-i = 0
-n = KNeighborsClassifier(n_neighbors=k)
-n.fit(X, y)
-print(f"test set: \n{test_set}")
-close = n.kneighbors(test_set)
-plt.plot(x0,x1, color="r")
-while i < k:
-    print(f"distances: {close[0][i]}\nindexes: {close[1][i]}")
-    i += 1
-plt.show()
+def run_test(k, data):
+    print(f"------\n| k == {k} |\n-------")
+    X = data[0]
+    x0, x1 = X[:, [0, 1]]
+    y = data[1]
+    n = KNeighborsClassifier(n_neighbors=k)
+    n.fit(X, y)
+    close = n.kneighbors(test_set)
+    plt.plot(x0,x1, color="r")
 
+def simulate(k, data):
+    for i in k: # iterate the list
+        run_test(i, data)
+
+
+test_data = open_csv_file(path)
+plot_data(test_data)
 
 
