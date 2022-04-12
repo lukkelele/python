93a5b0425f88a21e6b464364cf223e17bc0bc4c3
lukkelele <encryptedluke@protonmail.com>
2022-04-12 13:41:00 +0200

    update

--- school/2DV516/a2/a.py
+++ school/2DV516/a2/a.py
@@ -5,14 +5,27 @@ import csv_parser as csv
 import numpy as np
 import math
 
+# Column 1: girl height
+# Column 2: mom height
+# Column 3: dad height
 path = "./data/girls_height.csv"
 
 class Exercise_A:
 
     def __init__(self, path):
         self.dataset = csv.open_csv_file(path)
+        print(self.dataset)
 
     def plot_dataset(self):
         plt.subplot(2,2,1)
         plt.title("Dataset")
-        plt.xlabel('')
+        plt.xlabel('mom height')
+        plt.ylabel('dad height')
+        mom = self.dataset[:, 1]
+        dad = self.dataset[:, 2]
+        print(f"Mom: {mom}\n\n\n\nDad: {dad}")
+
+
+
+a = Exercise_A(path=path)
+a.plot_dataset()
--- school/2DV516/a2/csv_parser.py
+++ school/2DV516/a2/csv_parser.py
@@ -1,12 +1,22 @@
 import numpy as np
 import csv
 
+#path = "./data/girls_height.csv"
+
+path = "./data/girls_height.csv"
+
+
 def open_csv_file(path):
     try:
         with open(path) as csv_data:
             r = csv.reader(csv_data)
             X = []
-            for row in r: # row[0] == x0_val  | row[1] == x1_val  | row[2] == y_val
-                X.append([row[0], row[1]], row[2])
+            for row in r: 
+                row = row[0].split('\t')
+                #print(f"{row[0]}\n{row[1]}\n{row[2]}")
+                X.append([row[0], row[1], row[2]])
             return np.array(X, dtype=float)
     except: print("An error has occured!")
+
+data = open_csv_file(path)
+print(data)
