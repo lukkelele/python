78becc9aeeed84c434038b8ad844c65aef6de6f2
lukkelele <encryptedluke@protonmail.com>
2022-04-12 13:56:00 +0200

    update

--- school/2DV516/a2/a.py
+++ school/2DV516/a2/a.py
@@ -14,17 +14,21 @@ class Exercise_A:
 
     def __init__(self, path):
         self.dataset = csv.open_csv_file(path)
-        print(self.dataset)
 
     def plot_dataset(self):
+        f = plt.figure(figsize=(12,9))
         plt.subplot(2,2,1)
         plt.title("Dataset")
         plt.xlabel('mom height')
         plt.ylabel('dad height')
-        mom = self.dataset[:, 1]
-        dad = self.dataset[:, 2]
-        print(f"Mom: {mom}\n\n\n\nDad: {dad}")
-
+        y = self.dataset[:, 0]
+        x0 = self.dataset[:, 1]
+        x1 = self.dataset[:, 2]
+        x0_min, x1_min = np.min(x0), np.min(x1)
+        x0_max, x1_max = np.max(x0), np.max(x1)
+        plt.scatter(x0, x1)
+        plt.show()
+        
 
 
 a = Exercise_A(path=path)
--- school/2DV516/a2/csv_parser.py
+++ school/2DV516/a2/csv_parser.py
@@ -1,10 +1,6 @@
 import numpy as np
 import csv
 
-#path = "./data/girls_height.csv"
-
-path = "./data/girls_height.csv"
-
 
 def open_csv_file(path):
     try:
@@ -13,10 +9,7 @@ def open_csv_file(path):
             X = []
             for row in r: 
                 row = row[0].split('\t')
-                #print(f"{row[0]}\n{row[1]}\n{row[2]}")
                 X.append([row[0], row[1], row[2]])
             return np.array(X, dtype=float)
     except: print("An error has occured!")
 
-data = open_csv_file(path)
-print(data)
