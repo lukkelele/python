c88a62f334b1ebeb916d0ed582cfb83c8454b2d8
lukkelele <encryptedluke@protonmail.com>
2022-04-12 15:05:09 +0200

    update

--- school/2DV516/a2/a.py
+++ school/2DV516/a2/a.py
@@ -10,20 +10,25 @@ import math
 # Column 3: dad height
 path = "./data/girls_height.csv"
 
+# Linear Regression | y = a + bx 
+# (yi - y)^2 = (yi - (a + bx))^2  | the distance
+# Cost function, J | (1/n)*SUM(yi - (a+bx))^2  |  minimize the average distance 
+# J(a, b) = (1/n)*SUM(yi - (a+bx))^2 
+# dy/dx = 0  ==> extreme values (max, min)
 class Exercise_A:
 
     def __init__(self, path):
         self.dataset = csv.open_csv_file(path)
 
     def plot_dataset(self):
-        f = plt.figure(figsize=(12,9))
-        plt.subplot(2,2,1)
-        plt.title("Dataset")
-        plt.xlabel('mom height')
-        plt.ylabel('dad height')
+        #f = plt.figure(figsize=(12,9))
+        ax = plt.subplot(2,2,1)
+        ax.set_title("Dataset")
+        ax.set_xlabel('mom height')
+        ax.set_ylabel('dad height')
         y = self.dataset[:, 0]
-        x0 = self.dataset[:, 1]
-        x1 = self.dataset[:, 2]
+        #x0 = self.dataset[:, 1]
+        #x1 = self.dataset[:, 2]
         X = self.dataset[:, [1,2]]
         #x0_min, x1_min = np.min(x0), np.min(x1)
         #x0_max, x1_max = np.max(x0), np.max(x1)
