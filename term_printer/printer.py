e6e808ca5008a8f70a4a5f166a57879ae0e7d567
lukkelele <encryptedluke@protonmail.com>
2022-04-13 16:48:26 +0200

    update

--- term_printer/printer.py
+++ term_printer/printer.py
@@ -7,11 +7,17 @@ class Printer:
     def __init__(self, path):
         self.path = path
         self.r = open(self.path)
-        
+        self.queue = []
+
     def parse_text(self):
         for line in self.r:
-            if line != "":
-                print(line, end="")
+            if line.startswith("$:"):
+                txt = ""
+                lines = 0
+                while self.r.readline() != "":
+                    txt += self.r.readline()
+                    lines += 1
+                    print(line)
 
 
 p = Printer('./muddy_sea.txt')
