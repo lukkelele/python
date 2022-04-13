fe60ae792c1ad381500cbc72a87523ed602ec238
lukkelele <encryptedluke@protonmail.com>
2022-04-13 17:16:09 +0200

    update

--- term_printer/printer.py
+++ term_printer/printer.py
@@ -8,6 +8,9 @@ class Printer:
         self.path = path
         self.r = open(self.path)
         self.text = self.separate_text()
+        self.default_order = 0
+        self.queue = []
+        self.non_priority = []
 
     def separate_text(self):
         txt = ""
@@ -17,7 +20,20 @@ class Printer:
 
     def parse_text(self):
         for part in self.text:
-            print(part)
+            if part.startswith(':'):
+                queue_spot = part[1]
+                self.queue.append([queue_spot, part])
+            else:
+                if part != "":
+                    self.non_priority.append([self.default_order, part])
+                    self.default_order += 1
+        priority_order = len(self.queue)
+        non_priority_order = len(self.non_priority)
+        print(f"Priority len: {priority_order}\nnon-priority: {non_priority_order}")
+
+
+
+
 
 
 p = Printer('./muddy_sea.txt')
