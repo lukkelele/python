366d623a7ae0a06be3e959f4e1b063d8b4f32a61
lukkelele <encryptedluke@protonmail.com>
2022-04-14 13:23:36 +0200

    update

--- school/2DV516/a2/a.py
+++ school/2DV516/a2/a.py
@@ -34,21 +34,19 @@ class Exercise_A:
 
     # Standard deviation --> compute each value - mean
     def feature_norm(self):
-        mom_height = self.X[:, 0]
-        dad_height = self.X[:, 1]
+        mom_height, dad_height = self.X[:, 0], self.X[:, 1]
         mom_mean, dad_mean = np.mean(mom_height), np.mean(dad_height)
         mom_std, dad_std = np.std(mom_height), np.std(dad_height)     
         mom_subt, dad_subt = np.subtract(mom_height, mom_mean), np.subtract(dad_height, dad_mean)
         mom_norm, dad_norm = np.divide(mom_subt, mom_std), np.divide(dad_subt, dad_std)
+        print(mom_norm/mom_std)
         Xn = np.array([mom_norm, dad_norm]).reshape((214,2))
         Xn_e = self.extend_x(Xn, len(dad_height))
-        print(f"\n|== Standard deviation ==|\nMom: {np.std(Xn_e[1])}\nDad: {np.std(Xn_e[2])}\n"
-             +f"\n|== Mean ================|\nMom: {np.mean(Xn_e[1])}\nDad: {np.mean(Xn_e[2])}\n") 
-        plt.scatter(Xn_e[:, 1], self.y, color="r", s=30, label='mom')
-        plt.scatter(Xn_e[:, 2], self.y, color="b", s=30, label='dad')
-        print(self.calc_beta(Xn_e))
-        print(self.calc_height(Xn_e[:,1], dad)(Xn_e))
-
+        print(f"\n|== Mean ================|\nMom: {mom_mean}\nDad: {dad_mean}\n"
+             +f"\n|== Height - mean =======|\nMom: {mom_subt}\nDad: {dad_subt}\n"
+             +f"\n|== Standard deviation ==|\nMom: {mom_std}\nDad: {dad_std}\n"
+             +f"\n|== Standard dev v2 =====|\nMom: {np.std(Xn_e[1])}\nDad: {np.std(Xn_e[2])}\n"
+             +f"\n|== Normalized Mean =====|\nMom: {np.mean(Xn_e[1])}\nDad: {np.mean(Xn_e[2])}\n") 
 
     # Normal equation
     def calc_beta(self, Xe):
@@ -65,8 +63,8 @@ class Exercise_A:
         print(f"Cost J: {J}\nlength_J: {len(J)}")
         return J
 
-    def calc_height(self, mom, dad):
-        height = self.beta[0] + self.beta[1]*mom + self.beta[2]*dad
+    def calc_height(self, beta, mom, dad):
+        height = beta[0] + beta[1]*mom + beta[2]*dad
         print(f"Calculated height: {height}")
         return height
 
