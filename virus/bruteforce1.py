4c2e0424128eec7c13146ca6eed8ebbecbcdbb1e
lukkelele <encryptedluke@protonmail.com>
2022-04-16 13:05:49 +0200

    update

--- virus/bruteforce1.py
+++ virus/bruteforce1.py
@@ -6,12 +6,26 @@ class Doorknock:
         self.n = n
 
     def get_chars(self):
-        self.chars = [chr(i) for i in range(33, 127)]
-        self.chars.remove('\\')
-        print(self.chars)
+        self.chars = [chr(i) for i in range(47, 56)]
+        #self.chars.remove('\\')
 
-
+    def crack(self, n):
+        passwd = []
+        L = []
+        for i in range(n):
+            passwd.append(self.chars[0])
+        for c in passwd:
+            idx = passwd.index(c)
+            print(f"CURRENT IDX: {idx}\nc: {c}")
+            for x in range(len(self.chars)):
+                for char in self.chars:
+                    passwd[idx] = char
+                    L.append(passwd)
+                    print(passwd)
+                passwd[idx+1] = self.chars[x]
+        print(f"len(chars): {len(self.chars)}\n{len(self.chars)**2}\nlen(L): {len(L)}")
 
 
 D = Doorknock(4)
 D.get_chars()
+D.crack(2)
