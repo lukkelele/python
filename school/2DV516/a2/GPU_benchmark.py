70cff429b8805c74d4b562daae6027dd8f9a8368
lukkelele <encryptedluke@protonmail.com>
2022-04-18 20:13:01 +0200

    gpu benchmark exercise update

--- school/2DV516/a2/GPU_benchmark.py
+++ school/2DV516/a2/GPU_benchmark.py
@@ -1,6 +1,8 @@
+from matplotlib import pyplot as plt
 import mlrlib as func
 import numpy as np
 import csv_parser
+
 
 # The 7th column is the response y
 # Find f(X) = B0 + B1*X1 + ... + B6*X6
@@ -12,6 +14,7 @@ class GPU_benchmark:
     def __init__(self, path):
         self.path = path
         self.parse_csv_file()
+        self.fig = plt.figure(figsize=(12,9))
 
     def parse_csv_file(self):
         dataset = csv_parser.open_gpu_file(self.path)
@@ -27,11 +30,18 @@ class GPU_benchmark:
     def normalize_X(self, X):
         col_length = len(X[:,0])
         Xn = func.normalize_column(X, 0).reshape(col_length, 1)
-        for i in range(2, 6):
+        for i in range(1, 6):
             xn = func.normalize_column(X, i).reshape(col_length, 1)
             Xn = np.concatenate((Xn, (xn)), axis=1)
         return Xn
 
+    def plot_features(self, X, y):
+        for i in range(6):
+            plt.subplot(2, 3, i+1)
+            plt.scatter(X[:, i], y, s=10, color="b")
+
 
 g = GPU_benchmark(csv_path)
 print(g.normalize_X(g.X))
+g.plot_features(g.X, g.y)
+#plt.show()
