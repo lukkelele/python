81f67ce7a85fe792456224841ab13cec6ed1358c
lukkelele <encryptedluke@protonmail.com>
2022-04-23 08:06:24 +0200

    breast cancer project added

---
+++ school/2DV516/a2/breast_cancer.py
@@ -0,0 +1,37 @@
+from matplotlib import pyplot as plt
+from lib import csv_parser
+import mlrlib as func
+import numpy as np
+
+csv_path = "./data/breast_cancer.csv"
+
+# Divide 80/20
+
+class Cancer:
+    
+    def __init__(self, path):
+        self.fig = plt.figure(figsize=(12,9))
+        self.parse_csv_file(path)
+        self.adjust_response()
+
+    def parse_csv_file(self, path):
+        dataset = csv_parser.open_cancer_file(path)
+        np.random.shuffle(dataset)
+        self.n = len(dataset)
+        self.X = dataset[:,[0,1,2,3,4,5,6,7,8]]
+        self.y = dataset[:, 9]
+        self.create_extended_matrixes()
+
+    def create_extended_matrixes(self):
+        matrixes = func.create_extended_matrixes(self.X)
+        self.Xn, self.Xe, self.Xn_e = matrixes[0], matrixes[1], matrixes[2]
+
+    def adjust_response(self):
+        idx = 0
+        for response in self.y:
+            if response == 2: self.y[idx] = 0
+            else: self.y[idx] = 1
+            idx += 1
+
+c = Cancer(csv_path)
+
--- school/2DV516/a2/lib/csv_parser.py
+++ school/2DV516/a2/lib/csv_parser.py
@@ -43,3 +43,15 @@ def open_admission_file(path):
                 X.append([row[0], row[1], row[2]])
             return np.array(X, dtype=float)
     except: print("An error has occured!")
+
+def open_cancer_file(path):
+    try:
+        with open(path) as csv_data:
+            r = csv.reader(csv_data)
+            X = []
+            for row in r: 
+                X.append([row[0],row[1],row[2],row[3],
+                          row[4],row[5],row[6],row[7],
+                          row[8],row[9]])
+            return np.array(X, dtype=float) # dtype=int ?
+    except: print("An error has occured!")
