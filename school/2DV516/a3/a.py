78fbcc2d2af1381e1e8cecea862ab991bc5a879d
lukkelele <encryptedluke@protonmail.com>
2022-05-06 14:22:12 +0200

    update

--- school/2DV516/a3/a.py
+++ school/2DV516/a3/a.py
@@ -5,22 +5,26 @@ import pandas as pd
 import numpy as np
 
 path = './data/bm.csv'
+SAMPLE_SIZE = 5000
 
 class a:
 
     def __init__(self, path):
         cols = ['x0', 'x1', 'y']
-        data = pd.read_csv(path, names=cols) 
-        print(data)
+        data = pd.read_csv(path).values
+        print(type(data))
+        self.X = data[:,[0,1]] 
+        self.y = data[:,2]
+        self.x0 = data[:,0]
+        self.x1 = data[:,1]
 
     def generate_sample(self, datapoints):
         np.random.seed(7)
         r = np.random.permutation(datapoints)
-        
-
+        X_s, y_s = X[:datapoints, :], y[:datapoints]
+        print(len(X_s))
 
 
 
 a = a(path)
-
-a.generate_sample(5000)
+a.generate_sample(SAMPLE_SIZE)
