30a1e63648d84c2fe3a1ca9cc2cf7f97a1ab6f3e
lukkelele <encryptedluke@protonmail.com>
2022-05-11 15:24:26 +0200

    update

--- school/2DV516/a3/kernels.py
+++ school/2DV516/a3/kernels.py
@@ -13,26 +13,23 @@ import numpy as np
 # Linear ==> C=1
 # Poly ==> C=1, degree=1, gamma=1
 # RBF ==> C=1000, gamma=0.01
-# Split the data 
+# 2D to determine a number (1, 3, 5, 9)
 
 path = './data/mnistsub.csv'
-# Linear: C | RBF: C, gamma | Poly: C, degree, gamma
-params = [param_linear, param_rbf, param_poly]
-optimized_params = [[1], [1000, 0.01], [1, 1, 1]] 
-
+# Linear: C | RBF: C, gamma | Poly: C, degree, gamma |
+#   idx 0   |   idx 0, 1    |      idx 0, 1, 2       | 
 param_linear = {'C': [0.1, 1, 5, 10, 50, 100, 1000],
-                'kernel': ['linear']        
-             }
+                'kernel': ['linear']    }
 param_rbf  = {'C': [0.1, 1, 10, 100, 1000, 10000],
              'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
-             'kernel': ['rbf']
-             }
+             'kernel': ['rbf']      }
 param_poly = {'C': [0.1, 1, 10, 100, 1000],
              'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
              'degree': [1, 2, 3, 4],
-             'kernel': ['poly']
-             }
+             'kernel': ['poly']     }
 
+params = [param_linear, param_rbf, param_poly]
+optimized_params = [[1], [1000, 0.01], [1, 1, 1]] 
 
 class Kernel:
 
@@ -42,12 +39,18 @@ class Kernel:
         self.X = data[:,[0,1]] 
         self.Y = data[:,2]
         if tune_params: self.tune_hyperparams(X, Y)
-        else: self.param_linear, self.param_rbf, self.param_poly = params[0], params[1], params[2]
+        else: self.param_linear, self.param_rbf, self.param_poly = optimized_params[0], optimized_params[1], optimized_params[2]
+        self.create_classifiers(verbose=True)
+        print(self.X.shape)
+        self.clf_linear.fit(self.X, self.Y)
 
-    def create_classifiers(self, ):
-        clf_linear = a3.get_classifier(kernel='linear')
-
-    def tune_hyperparams(self, X, Y, verbose=0):
+    def create_classifiers(self, verbose=False):
+        self.clf_linear = svm.SVC(kernel='linear', C=self.param_linear[0])
+        self.clf_poly = svm.SVC(kernel='poly', C=self.param_poly[0], degree=self.param_poly[1], gamma=self.param_poly[2])
+        self.clf_rbf = svm.SVC(kernel='rbf', C=self.param_rbf[0], gamma=self.param_rbf[1])
+        if verbose: print(f"All classifiers created!\nlinear: {self.clf_linear}\nrbf: {self.clf_rbf}\npoly: {self.clf_poly}")
+        
+    def tune_hyperparams(self, X, Y):
         for param in params:
             start = time()
             grid_search = GridSearchCV(svm.SVC(), param, refit=True)
@@ -57,9 +60,19 @@ class Kernel:
             else: print(f"Grid search for {param['kernel']} completed in {time_spent} seconds")
             print(grid_search.best_params_)
 
+    def plot_data(self, X, Y):
+        plt.subplot(131)
+        plt.scatter(X[:,0], X[:,1], s=12, edgecolors='k') 
+        #self.clf_linear.predict([X[:,0], X[:,1]])
+        plt.subplot(132)
+        plt.scatter(X[:,0], X[:,1], s=12, edgecolors='k')
+        plt.subplot(133)
+        plt.scatter(X[:,0], X[:,1], s=12, edgecolors='k')
+
+
+
 
 
 k = Kernel(path)
-
-
-print("end")
+k.plot_data(k.X, k.Y)
+plt.show()
