f3922156f006bfeade365a58fa21756279fe14c5
lukkelele <encryptedluke@protonmail.com>
2022-05-13 09:16:35 +0200

    update

--- school/2DV516/a3/one_versus_all.py
+++ school/2DV516/a3/one_versus_all.py
@@ -25,43 +25,37 @@ optimized_gamma = 0.001
 
 class OneVersusAll:
 
-    def __init__(self, path, tune_params=False, train_size=0.20, test_size=0.05, verbose=True):
+    def __init__(self, path, tune_params=False, test_size=0.15, verbose=True):
         print("\nSTARTING SCRIPT...")
         self.fig = plt.figure(figsize=(20,11))
-        self.load_mnist_data(train=train_size, test=test_size, verbose=verbose)
+        self.load_mnist_data(test=test_size, verbose=verbose)
         if tune_params == True: self.tune_hyperparams(self.x_train, self.y_train) # only prints best, does not set them
         print("==> Creating classifier...")
         self.clf_rbf = svm.SVC(kernel='rbf', C=optimized_C, gamma=optimized_gamma)
-        print("  Classifier CREATED!\n==> Training model...")
+        print("    Classifier CREATED!\n==> Training model...")
         self.clf_rbf.fit(self.x_train, self.y_train)
-        print("  Model successfully trained!")
+        print("    Model successfully trained!")
 
-    def load_mnist_data(self, train=0.10, test=0.05, verbose=False):
-        if verbose: print(f"==> Loading MNIST...\n  TRAINING: {train*100}%\n  TESTING: {test*100}%")
+    def load_mnist_data(self, test=0.05, verbose=True):
+        if verbose: print(f"==> Loading MNIST...\n    TRAINING: {(1-test)*100}%\n    TESTING: {test*100}%")
         start = time()
+        X, Y = datasets.fetch_openml("mnist_784", version=1, return_X_y=True, as_frame=False)
+        X = X.reshape((X.shape[0], -1))
         numbers = datasets.load_digits()
         n_samples = len(numbers.images)
         data = numbers.images.reshape((n_samples, -1))
-        #(X, y), (xx_test, yy_test) = mnist.load_data()
-        #x_train, x_test, self.y_train, self.y_test = train_test_split(X, y, train_size=train, test_size=test)
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(data, numbers.target, test_size=0.25, shuffle=False)
-        if verbose: print("==> Data SPLIT!\n  Beginning to reshape training and test data...")
-        #n, nx, ny = x_train.shape
-        #self.x_train = x_train.reshape((n,nx*ny))
-        #n, nx, ny = x_test.shape
-        #self.x_test = x_test.reshape((n, nx*ny))
-        if verbose: print(f"  Time spent loading MNIST dataset: {round((time()-start), 2)} seconds")
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(data, numbers.target, test_size=test, shuffle=False)
+        if verbose: print("==> Data SPLIT!\n    Beginning to reshape training and test data...")
+        if verbose: print(f"    Time spent loading MNIST dataset: {round((time()-start), 2)} seconds")
 
-    def evaluate_model(self):
+    def evaluate_model(self, conf_matrix=False):
         print("==> Beginning to evaluate model...")
         pred_y = self.clf_rbf.predict(self.x_test)
-        actual = self.y_test
-        #conf_matrix = metrics.confusion_matrix(actual, pred)
-        clf_report = metrics.classification_report(actual, pred_y)
-        print(clf_report)
+        if conf_matrix: print(f"==> Confusion Matrix: \n{metrics.confusion_matrix(self.y_test, pred)}\n"+
+                f"==> Classification report: \n{metrics.classification_report(self.y_test, pred_y)}\n")
         score = round((self.clf_rbf.score(self.x_test, self.y_test)), 6)
         error = round((1-score), 6)
-        print(f"  Accuracy: {score*100}%\n  Error: {error*100}%\n----------------------------\n")
+        print(f"    FINAL RESULTS\n    Accuracy: {score*100}%\n    Error: {error*100}%\n\n----------------------------\n\n")
 
     def tune_hyperparams(self, X, Y):
         start = time()
@@ -72,8 +66,7 @@ class OneVersusAll:
         else: print(f"Grid search for RBF completed in {time_spent} seconds")
         print(grid_search.best_params_)
 
-one = OneVersusAll(path, train_size=0.12, test_size=0.05, tune_params=False)
+one = OneVersusAll(path, test_size=0.05, tune_params=False)
 #one.tune_hyperparams(one.x_train, one.y_train)
 one.evaluate_model()
 
-print(len(one.x_test))
