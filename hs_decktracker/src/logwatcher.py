1733ef828ddf378fb02a776d7e5d0aa4299d0585
lukkelele <encryptedluke@protonmail.com>
2022-06-16 13:15:21 +0200

    update

--- hs_decktracker/src/logwatcher.py
+++ hs_decktracker/src/logwatcher.py
@@ -10,35 +10,32 @@ class Logwatcher:
 
     def __init__(self, path):
         self._cached_stamp = 0
-        self.logfile = self.open_log(path)
-        self.file = path
+        self.path = path
         self.linecount = self.get_linecount()
 
-    def open_log(self, path):
-        log = open(f"{path}", 'r')
-        return log
-
-    def read_log(self, file):
-        for line in file:
-            match = re.search("zone=PLAY", line)
-            if match is not None:
-                print(line, end="\n")
-
     def poll(self):
-        stamp = os.stat(self.file).st_mtime
+        stamp = os.stat(self.path).st_mtime
         if stamp != self._cached_stamp:
             self._cached_stamp = stamp
             linecount = self.get_linecount()
             # File changed, iterate through new lines 
             k = 0
             d = linecount - self.linecount   # new - old
+            print(f"Linecount==> {linecount}\nd == {d}")
             while k < d:
-                line = self.logfile[self.linecount + k] # Newly added line in logfile
+                print(f"Current k: {k}")
+                logfile = open(self.path)
+                file = logfile.readlines()
+                line = file[self.linecount + k] # Newly added line in logfile
                 # Parse new lines to see what kind of information they contain
+                print(f"polled line: {line}")
+                logfile.close()
+                k+=1
+            k=0
             self.linecount = linecount # Update the linecount
 
     def get_linecount(self):
-        p = subprocess.Popen(['wc', '-l', self.file], stdout=subprocess.PIPE,
+        p = subprocess.Popen(['wc', '-l', self.path], stdout=subprocess.PIPE,
                                                       stderr=subprocess.PIPE)
         result, err = p.communicate()
         if p.returncode != 0:
@@ -55,5 +52,5 @@ class Logwatcher:
 
 l = Logwatcher("../test/log_test.txt")
 print(l.linecount)
-r = l.logfile.readlines()
-l.handle_event(r[1163])
+while True:
+    l.poll()
