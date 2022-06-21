309223f32d170f781e8eaffb1885e48c3a3849ba
lukkelele <encryptedluke@protonmail.com>
2022-06-21 18:50:09 +0200

    Further cleaning

--- hs_decktracker/src/EventHandler.py
+++ hs_decktracker/src/EventHandler.py
@@ -6,6 +6,7 @@ import re
 class EventHandler:
 
     def __init__(self):
+        self.blacklist = []
         print("EventHandler created")
 
     def card_drawn(self, event):
@@ -23,22 +24,26 @@ class EventHandler:
         cardId = self.getVal("cardId=", line)
         return cardId
     
+    # Get the start of a game.
+    # Might have to blacklist the linecounts after each fetch.
+    # Consecutive games will have a log that has multiple gamestarts.
     def getGameStart(self, logfile):
         linecount = 1
         for line in logfile:
             game_start = re.search('Gameplay.Awake()', line)
-            if game_start != None:
+            if game_start != None and linecount not in self.blacklist:
+                self.blacklist.append(linecount)    # keep track of used linecounts
                 return linecount 
             linecount += 1
         return None
         
+    # TODO: Change function name and consider its use at all..
     # Get the m_id. Odd --> Player without coin
     def checkPlayerTurn(self, line, player: Player):
         print('Checking turn')
         turn_complete = True if re.search('m_complete=True', line) != None else False
         if turn_complete:
             m_id = int(self.getVal("m_id=", line))
-            if player.coin and m_id % 2 != 0: # if player has coin and m_id is odd
-                return False    # Opponent turn
-            else: return True
+            if player.coin and m_id % 2 != 0: return False    # Opponent turn
+            else: return True   # Player turn
 
