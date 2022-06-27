5dfd38f7e6b656b591137b4515fb8fcbc90b5b81
lukkelele <encryptedluke@protonmail.com>
2022-06-27 20:31:06 +0200

    Update

--- hs_decktracker/test/database_test.py
+++ hs_decktracker/test/database_test.py
@@ -7,9 +7,13 @@ cardIds = {'SW_433':'Seek Guidance'}
 db = CardDB.CardDB(verbose=True) 
 def test_fetchCardNames(): 
     for cardId in cardIds:
-        cardName = db.fetchCardName(cardId)
+        cardName = db.fetchCardName(cardId, verbose=True)
         assert cardIds[cardId] == cardName, f"should be {cardIds[cardId]}"
 
+def test_getCardStats():
+    for cardId in cardIds:
+        db.getCardStats(cardId)
 
 test_fetchCardNames()
+test_getCardStats()
 print("Testing successful!")
