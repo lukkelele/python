2c736081a8326e5fcc0b35da9d0677d312caee90
lukkelele <encryptedluke@protonmail.com>
2022-06-29 21:51:02 +0000

    java update

--- hs_decktracker/src/main/LogReader.java
+++ hs_decktracker/src/main/LogReader.java
@@ -1,10 +1,14 @@
-import com.google.gson.Gson;
+//import com.google.gson;
 
 
 public class LogReader {
 
   public LogReader() {
     System.out.println("LogReader object created!");
+  }
+
+  public static void main(String[] args) {
+    LogReader logReader = new LogReader();
   }
 
 }
