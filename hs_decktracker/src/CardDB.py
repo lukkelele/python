1eaf4c8ec58cbeb19ccbdeff0f70a8d882b14700
lukkelele <encryptedluke@protonmail.com>
2022-06-30 15:14:57 +0200

    Further development. some DBF ids are not available in the json file

--- hs_decktracker/src/CardDB.py
+++ hs_decktracker/src/CardDB.py
@@ -8,9 +8,10 @@ import Enums as Enum
 import json
 import time
 
+# SOME ISSUES WITH DBF ids .... 
+# Check out if lost cards exists in XML format
 
 class CardDB:
-
 
     def __init__(self):
         self.carddefs_path = hsdata.get_carddefs_path()
@@ -21,11 +22,6 @@ class CardDB:
     def getRoot(self):
         return ET.parse(self.carddefs_path).getroot()
     
-    # Get the value for a specific attribute within an entity.
-    def getAttributeVal(self, entity, enum):
-        try: return int(entity[enum.value].attrib['value'])
-        except: return None
-
     # TODO: Add automatic updates from the latest patches when they surface on the site.
     def getCardData(self):
         url = 'https://api.hearthstonejson.com/v1/121569/enUS/cards.json'
@@ -45,40 +41,44 @@ class CardDB:
                 json.dump(data, file, indent=2)
         return data
         
-
     def fetchCard(self, cardId):
         for card in self.db:
             if cardId == card['dbfId'] or cardId == card['id']:
-                cardAttack, cardHealth = None, None
-                if isinstance(cardId, int) == False: # if CardID and not DBF
-                    cardID = cardId
-                    cardDBF = card['dbfId']
-                else:
-                    cardDBF = cardId
-                    cardID = card['id']
-                cardName = card['name']
-                cardType = card['type']
-                cardCost = card['cost']
-                cardRarity = card['rarity']
-                cardSet = card['set']
-                cardDescription = card['text']
+                try:
+                    cardAttack, cardHealth = None, None
+                    # Check if the passed parameter 'cardId' is a string or an integer
+                    # String -> cardID  &  Integer -> DBF
+                    if isinstance(cardId, int) == False: # if CardID and not DBF
+                        cardID = cardId
+                        cardDBF = card['dbfId']
+                    else:
+                        cardDBF = cardId
+                        cardID = card['id']
+                    cardName = card['name']
+                    cardType = card['type']
+                    cardCost = card['cost']
+                    cardRarity = card['rarity']
+                    cardSet = card['set']
+                    cardDescription = card['text']
+                except: print(f"Couldnt find card by id {cardId}") ; return None, None, cardName, None, None, None, None, None
         return cardID, cardDBF, cardName, cardType, cardCost, cardAttack, cardHealth, cardRarity, cardDescription                
 
 
     def saveCard(self, cardId):
-        cardID, cardDBF, cardName, cardType, cardCost, cardAttack, cardHealth, cardRarity, cardDescription = self.fetchCard(cardId)
-        card = {
-                "cardId": cardID,
-                "DBF": cardDBF,
-                "name": cardName,
-                "cardType": cardType,
-                "cost": cardCost,
-                "attack": cardAttack,
-                "health": cardHealth,
-                "rarity": cardRarity,
-                "description": 0 # Description 
-                }
-        print(card)
+        try:
+            cardID, cardDBF, cardName, cardType, cardCost, cardAttack, cardHealth, cardRarity, cardDescription = self.fetchCard(cardId)
+            card = {
+                    "cardId": cardID,
+                    "DBF": cardDBF,
+                    "name": cardName,
+                    "cardType": cardType,
+                    "cost": cardCost,
+                    "attack": cardAttack,
+                    "health": cardHealth,
+                    "rarity": cardRarity,
+                    "description": 0 # Description 
+                    }
+        except: print(f"Couldn't save card by id {cardId}") ; card = None
         return card
 
     def saveDeck(self, deck):
@@ -95,7 +95,7 @@ class CardDB:
     def importDeck(self, deckString):
         print(f"Saving deck by deckstring {deckString}")
         deck = Deck.importDeck(deckString)
-        for card in deck.cards: print(f"{deck.cards.index(card)+1}. {card}")
+        #for card in deck.cards: print(f"{deck.cards.index(card)+1}. {card}")
         return deck.cards
 
     def convertDeck(self, deck: list):
@@ -108,10 +108,17 @@ class CardDB:
         return jsonDeck
 
 
-deckString1 = "AAECcAf0GBPXOBJ7UBJfUBMP5Aw38rASEoASPnwThpASk7wPboASRoAS9tgTL+QPWoASywQSd1ASkoAQA"
+deckString1 = "AAECAf0GBPXOBJ7UBJfUBMP5Aw38rASEoASPnwThpASk7wPboASRoAS9tgTL+QPWoASywQSd1ASkoAQA"
 deckThiefRouge = "AAECAaIHBqH5A/uKBPafBNi2BNu5BIukBQyq6wP+7gOh9AO9gAT3nwS6pAT7pQTspwT5rASZtgTVtgT58QQA"
 
 db = CardDB()
-#db.fetchCard('YOP_035')
 db.saveCard('YOP_035')
-db.saveCard(61973)
+#db.saveCard(61973)
+#db.saveCard(66939)
+deck1 = db.importDeck(deckString1)
+convertDeck1 = db.convertDeck(deck1)
+#rogueDeck = db.importDeck(deckThiefRouge)
+#convertedRogueDeck = db.convertDeck(rogueDeck)
+print(convertDeck1)
+
+
