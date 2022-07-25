d801cd98b88621ae14e3e0dedb7d7e182637a011
lukkelele <encryptedluke@protonmail.com>
2022-07-25 06:44:16 +0200

    Update on scikit task

--- school/a1/scikit_knn.py
+++ school/a1/scikit_knn.py
@@ -13,22 +13,38 @@ chip_2 = [-0.5, -0.1]
 chip_3 = [ 0.6,    0]
 X_test = [chip_1, chip_2, chip_3]
 
+def output_result(chip, y_pred: int):
+    print(f">> Chip {chip}", end=" ")
+    if y_pred == 0: print("=> Fail\n")
+    else: print("=> OK!\n")
+
 # Read data
 data = ml.open_csv_file('./data/microchips.csv')
 X, y = data[:,[0,1]], data[:,2]
 fig = plt.figure(figsize=(14,12))
 plt.suptitle('Classification using KNN', fontsize=20)
 
-for k in K:
-    clf = KNeighborsClassifier(n_neighbors=k)
-    clf.fit(X, y)
-    Y_pred = clf.predict(X_test)
-    idx = 0
-    for y in Y_pred:
-        print(f"> Chip {idx+1} => {X_test[idx]}")
-        idx+=1
-    print("------------------")
+# Create the classifiers for the different k's
+clf1 = KNeighborsClassifier(n_neighbors=1)
+clf2 = KNeighborsClassifier(n_neighbors=3)
+clf3 = KNeighborsClassifier(n_neighbors=5)
 
+# Train the classifiers
+clf1.fit(X, y), 
+clf2.fit(X, y)
+clf3.fit(X, y)
+
+# Predict the values
+prediction_k1 = clf1.predict(X_test)
+prediction_k2 = clf2.predict(X_test)
+prediction_k3 = clf3.predict(X_test)
+predictions = [prediction_k1, prediction_k2, prediction_k3]
+
+print(f">> PREDICTED VALUES")
+for x in X_test:
+    idx = X_test.index(x)
+    y_pred = predictions[idx]
+    output_result(x, y_pred)
 
 
 
