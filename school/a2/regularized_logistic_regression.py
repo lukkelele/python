5d77d43de460a177652bf993a2384f46e97045a7
lukkelele <encryptedluke@protonmail.com>
2022-08-10 18:37:49 +0200

    regularization log reg, need some cleaning

--- school/a2/regularized_logistic_regression.py
+++ school/a2/regularized_logistic_regression.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from matplotlib.colors import ListedColormap
+from matplotlib.ticker import MultipleLocator
 from matplotlib import pyplot as plt
 from matplotlib import colors
 from math import sqrt, floor
@@ -27,12 +28,12 @@ grid = np.c_[x1, x2]
 
 C = 10000
 
-def plot3x3_clfs(C):
-    #plt.figure()
+def plot3x3_clfs(C, plot=True):
+    plt.figure()
     cross_vals_scores = []
     errors = []
     for i in range(1, 10):
-        #plt.subplot(3,3,i)
+        if plot: plt.subplot(3,3,i)
         logReg = LogisticRegression(solver='lbfgs', C=C, tol=(1/np.e))
         # Create new X with the polynomial degree of i
         Xp = ml.polynomial(X1, X2, i, ones=False)
@@ -44,45 +45,57 @@ def plot3x3_clfs(C):
         y_pred = clf.predict(Xp)
         training_errors = np.sum(y!=y_pred)
         # Use the classifier on the mesh to get the decision boundary
-        p = clf.predict(X_mesh)
-        p_mesh = p.reshape(xx.shape)
-        cmap_light = ListedColormap(['#d07e7e', '#c5c5c5', '#54d650']) # mesh plot
-        #plt.pcolormesh(xx, yy, p_mesh, cmap=cmap_light)
-        # Plot the initial scatter plot on top of the decision boundary
-        #ml.log_plot_twofeature(X1, X2, y, training_errors)
+        if plot:
+            p = clf.predict(X_mesh)
+            p_mesh = p.reshape(xx.shape)
+            cmap_light = ListedColormap(['#d07e7e', '#c5c5c5', '#54d650']) # mesh plot
+            plt.pcolormesh(xx, yy, p_mesh, cmap=cmap_light)
+            # Plot the initial scatter plot on top of the decision boundary
+            ml.log_plot_twofeature(X1, X2, y, training_errors)
         score = cross_val_score(clf, Xp, y, cv=5).mean()
         cross_vals_scores.append(score)
         errors.append(clf.score(Xp, y))
+    plt.suptitle(f"C == {C}")
     return cross_vals_scores, errors
+
+#plt.legend()
 
 C_1, errors_C1 = plot3x3_clfs(1)
 C_10K, errors_C10K= plot3x3_clfs(C)
-C_2K, errors_C2K= plot3x3_clfs(2000)
-plt.figure()
-
-degrees = np.arange(1, 10, 1)
-xmin, xmax = 0, 10
-bar_width = 0.25
+avg_errors_C1, avg_errors_C10K = 0, 0
 set_size = len(X)
+bar_width = 0.25
+degrees = np.arange(1, 10, 1)
+plt.figure()
 for d in degrees:
     error_C1 = (1-errors_C1[d-1]) * set_size
     error_C10K = (1-errors_C10K[d-1]) * set_size
-    error_C2K = (1-errors_C2K[d-1]) * set_size
-    #plt.plot([d,d], [0, error_C1], color='r')
-    plt.bar(d, error_C1, color='r', width=bar_width, edgecolor='k')
-    plt.bar(d+bar_width, error_C10K, width=bar_width, color='b', edgecolor='k')
-    plt.bar(d+2*bar_width, error_C2K, width=bar_width, color='g', edgecolor='k')
-    #plt.plot(d, error_C10K, color='b')
+    avg_errors_C1 += error_C1
+    avg_errors_C10K += error_C10K
+    plt.bar(d-(bar_width/2), error_C1, color='g', width=bar_width, edgecolor='k')
+    plt.bar(d+(bar_width/2), error_C10K, width=bar_width, color='r', edgecolor='k')
 
-#plt.ylabel('Cross Validation Score')
 plt.xlabel('Polynomial degree')
-plt.xlim([0, 10])
-plt.legend(['Unregularized', 'Regularized', 'Regularized, C=2000'])
+plt.legend(['Regularized', 'Unregularized'])
+plt.xticks(np.arange(0,10,1)), plt.yticks(np.arange(0,75,5))
+
+mean = len(degrees)
+print(f""">>  Results:
+
+  < Unregularized
+    C = 1
+    Score: {sum(errors_C1)/mean}
+    Errors (avg): {avg_errors_C1/mean}
+    ===========================================
+  < Regularized
+    C = 10K
+    Score: {sum(errors_C10K)/mean}
+    Errors (avg): {avg_errors_C10K/mean}
+    
+    Mean (len(degrees)) ==> {mean}
+
+""")
 plt.show()
-
-
-
-
 
 
 
