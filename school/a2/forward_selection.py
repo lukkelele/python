18d558c2c6172c967463b2840dbb0d7b60e8a8db
lukkelele <encryptedluke@protonmail.com>
2022-08-10 20:50:45 +0200

    recursive combination function added. will tweak it to compute MSE inside the loop

--- school/a2/forward_selection.py
+++ school/a2/forward_selection.py
@@ -15,6 +15,17 @@ np.set_printoptions(threshold=sys.maxsize)
 data = ml.open_csv_file('./data/GPUbenchmark.csv')
 X, y = data[:, :6], data[:, 6]
 
+def combination(arr, n):
+    if n == 0: return [[]]
+    l = []
+    for i in range(0, len(arr)):
+        m = arr[i]
+        rem = arr[i+1:]
+        remCombo = combination(rem, n-1)
+        for p in remCombo:
+            l.append([m, *p])
+    return l
+
 def forward_selection(X, y):
     """
     Forward selection algorithm
@@ -23,23 +34,16 @@ def forward_selection(X, y):
     """
     Xn_e = ml.extend_matrix(ml.normalize_matrix(X))
     b = ml.calc_beta(Xn_e, y)
-    null_model = []
-    models = []
     p = np.size(X[0]) + 1   # p features
-    for j in range(p):
-        for i in range(j, p):
-            m = []
-            m.append(b[i])
-            for k in range(i, p):
-                if i != k:
-                    m.append(b[k])
-            models.append(m)
-
+    models = []
+    for i in range(1, p+1):
+        combo = combination(b, i)
+        models.append(combo)
+        print(combo)
+        
     return models
 
 
 M = forward_selection(X, y)
 
-for m in M:
-    print(m)
-print(len(M))
+
