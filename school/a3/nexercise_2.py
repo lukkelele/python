9efb5a103af5f099c720211d0c30bd26259006d4
lukkelele <encryptedluke@protonmail.com>
2022-08-16 10:50:49 +0200

    Further update. almost done

--- school/a3/nexercise_2.py
+++ school/a3/nexercise_2.py
@@ -12,7 +12,7 @@ import csv
 import sys
 import ml
 
-#np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(threshold=sys.maxsize)
 
 # ONE VERSUS ALL MNIST
 # RBF kernel, 95% test accuracy
@@ -24,13 +24,12 @@ import ml
 print('>> Getting mnist_784 dataset...')
 X, y = fetch_openml('mnist_784', version=1, data_home="~/Software/datasets/", return_X_y=True)
 print('   Done!\n>> Splitting data...')
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.12, test_size=0.03)
-X_train, X_test = X_train, X_test
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.004, test_size=0.003)
+X_train, X_test = X_train.to_numpy(), X_test.to_numpy()
 train_size = len(X_train)
 test_size = len(X_test)
 #y_train, y_test = y_train.to_numpy(), y_test.to_numpy()
 y_train, y_test = y_train.astype(np.int8), y_test.astype(np.int8)
-#y_train, y_test = y_train[:, 1], y_test[:, 1] 
 
 print('>> Data read\n   Size of training set: %s instances\n   Size of test set: %s instances' % (train_size, test_size))
 # Train 9 binary classifiers
@@ -41,27 +40,30 @@ for i in range(0, 10):
     print('   Classifier: %d' % i)
     Y_train.append(y_train == i)
 print('   Done creating target data!\n>> Creating the classifiers for each class...')
+
 # Create the classifiers with corresponding target data
 # No loops used because it does not work properly with sklearn
 kernel = 'rbf'
 C = 1
-clf0 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[0])
-clf1 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[1])
-clf2 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[2])
-clf3 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[3])
-clf4 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[4])
-clf5 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[5])
-clf6 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[6])
-clf7 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[7])
-clf8 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[8])
-clf9 = SVC(kernel=kernel, C=C).fit(X_train, Y_train[9])
+prob = True
+clf0 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[0])
+clf1 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[1])
+clf2 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[2])
+clf3 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[3])
+clf4 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[4])
+clf5 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[5])
+clf6 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[6])
+clf7 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[7])
+clf8 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[8])
+clf9 = SVC(kernel=kernel, C=C, probability=prob).fit(X_train, Y_train[9])
 clfs = [clf0, clf1, clf2, clf3, clf4, clf5, clf6, clf7, clf8, clf9]
 
 #C_arr = np.array([0.1])
 #g_arr = np.array([0.01])
-C_arr = np.logspace(-3, 13, 4)
-g_arr = np.logspace(-3, 6, 3)
+C_arr = np.logspace(-3, 13, 6)
+g_arr = np.logspace(-3, 6, 6)
 params = { 'C': C_arr, 'gamma': g_arr, 'kernel': ['rbf']}
+
 start = ml.stopwatch()
 print('   Done!\n>> Creating SVC...')
 clf = SVC()
@@ -77,18 +79,39 @@ print(f"""
    Best estimator: {grid.best_estimator_}")
    Time spent: {ml.stopwatch(start)}
 """)
+
 # Compute the scores for the test set using the binary classifers
-clfs_scores = []
-# TODO: Iterate each sample and decide proper class, (nested loop)
+Y_pred = np.zeros((len(X_test), 1))
 for clf in clfs:
-    idx = clfs.index(clf)
-    y_pred = clf.predict(X_test)
-    errors = np.sum(y_pred != y_test)
-    acc = round(((test_size - errors) / test_size), 4)
-    clfs_scores.append(acc)
-    print(f"   {idx}: {acc}")
-clfs_scores = np.array(clfs_scores)
-best_idx = np.where(clfs_scores == np.max(clfs_scores))
-best_score = clfs_scores[best_idx]
-print(f">> Best score: {best_score}, classifier {best_idx[0]}")
+    # Predict_proba : [0] NO  | [1]  YES
+    y_pred = clf.predict_proba(X_test)[:,1].reshape(-1,1) # choose positive prediction column
+    Y_pred = np.append(Y_pred, y_pred, 1)
+
+# delete first 0 column
+Y_pred = np.delete(Y_pred, 0, 1)
+print(Y_pred)
+print(Y_pred.shape)
+
+YY_pred = []
+for pred in Y_pred:
+    pred_class = np.where(pred == np.max(pred))[0]
+    YY_pred.append(pred_class)
+    #print(f"> predicted class: {pred_class}")
+
+
+YY_pred = np.array(YY_pred).ravel()
+print(YY_pred.shape)
+errors =  np.sum(YY_pred != y_test)
+tot_acc = round(((test_size - errors) / test_size), 4)
+print(f"""
+
+    Final results:
+         Accuracy: 
+                  One-Versus-Rest classifier: {tot_acc}
+                  One-Versus-One  classifier: {test_acc}
+""")
+
+
+
+
 
