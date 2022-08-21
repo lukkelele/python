5643a326445d2a73fbdd8277b6d7eecb641eba55
lukkelele <encryptedluke@protonmail.com>
2022-08-21 15:22:11 +0200

    updated

--- school/a4/exercise_1.py
+++ school/a4/exercise_1.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.cluster import KMeans
 from matplotlib.colors import ListedColormap
 from sklearn.metrics import accuracy_score
 from matplotlib import pyplot as plt
@@ -8,7 +9,7 @@ import numpy as np
 import sys
 import ml
 
-np.set_printoptions(threshold=sys.maxsize)
+#np.set_printoptions(threshold=sys.maxsize)
 
 # Clustering
 # Implement 'Bisecting k-Means'
@@ -16,86 +17,49 @@ data = ml.open_csv_file('./data/microchips.csv')
 X, y = data[:,:2], data[:,2]
 fig = plt.figure(figsize=(12,12))
 
-def convert_points(points):
-    """
-    Point conversion
-    points: list in a 2D format
-    """
-    points = np.array(points)
-    if len(points.shape) == 1:
-        print('Reshaping')
-        points = points.reshape(-1, 1) 
-    return points
-
-def typecheck(arr):
-    """
-    Typecheck for input data
-    If 'arr' is a list, convert to a 2D numpy array
-    Else, do nothing
-    """
-    if isinstance(arr ,list):
-        arr = convert_points(arr)
-    return arr
-
 def calculate_sse(points):
     """
     Sum of squared errors
     """
-    points = typecheck(points)
     centroid = np.mean(points, 0)
     sum_errors = np.sum(np.linalg.norm(points-centroid, axis=1, ord=2))
     return sum_errors 
 
+def get_clusters(points):
+    kmeans = KMeans(n_clusters=2).fit(points)
+    labels = kmeans.labels_
+    most_frequent = np.bincount(labels).argmax()     # pyright : ignore
+    print(most_frequent)
+    indices = np.where(labels == 1)[0]
+    split1 = points[indices]
+    split2 = np.delete(points, indices, axis=0)
+    return split1, split2
 
-def k_means(points, k, n=5):
+def bkmeans(X, n, cmap='plasma'):
     """
-    Cluster points into k clusters, k >= 2
-    k clusters means k centroids
-    Initially place random centroids then,
-    calculate labels, replace centroid --> repeat
+    Bisect kmeans
+    Create one cluster initially
+    Sort the largest cluster in to two
+    Take largest cluster and split it -> REPEAT
+    Initially [ 0 ] 
+    Then -> [0, 1] -> [0, 1, 2] -> [1, 2, 3, 4] etc..
+    Should return the cluster indices for each observation
     """
-    points = typecheck(points) 
-    assert len(points) >= 2, "error: k must be larger or equal to 2"
-    
-    np.random.seed(4)
-    np.random.shuffle(points)
-    centroids = points[0:k, :]
-    print(f">> Intiial centroids \n{centroids}\n\n")
-
-    # Calculate points distance to each cluster
-    clusters = k * [None]
-    for i in range(n):
-        clusters = k * [None]
-        for point in points:
-            distances = []
-            for centroid in centroids:
-                dist = ml.euclidean_distance(centroid, point)
-                #dist = ml.euclidean_distance(point, centroid)
-                distances.append(dist)
-            distances = np.array(distances)
-            c_idx = np.where(distances == min(distances))[0][0] # cluster index
-            if clusters[c_idx] is None:     # can't use '==' here because of numpy implementation
-                clusters[c_idx] = point.reshape(1,2)
-            else:
-                clusters[c_idx] = np.vstack((clusters[c_idx], point))
-
-        # Calculate the mean of the centroids in the clusters
-        # Set this new mean to be the value for the centroids and recalculate distances
-        centroids = np.array([np.mean(c, 0) for c in clusters])
-    
-    # Plot the data
-    cmap = 'rainbow'
-    centroids_ = np.arange(0, len(centroids), 1)
-
-    for cluster in clusters:
-        idx = clusters.index(cluster)
-        plt.scatter(centroids[idx][0], centroids[idx][1], s=300, edgecolors='k', alpha=0.89)
-        plt.plot(cluster[:,0], cluster[:,1], 'o')
-    
+    print(f"SIZE OF X: {len(X)}")
+    c1, c2 = get_clusters(X)
+    clusters = [c1, c2]
+    for i in range(1, n):
+        for cluster in clusters:
+            sse_points = calculate_sse(cluster)            
+        # Take cluster with largest error
+        cluster = clusters.pop(np.argmax(sse_points))    #pyright: ignore
+        c = get_clusters(cluster)
+        clusters.extend(c)
+    return clusters
 
 
-# NORMALIZED DATA
-lst = [ [0.45, 0.6], [0.4, 0.3] , [0.1, -0.32] , [-0.24, 0.42]]
-k_means(X, 3)
-
+clusters = bkmeans(X, 5)
+cmap = 'plasma'
+for cluster in clusters:
+    plt.scatter(cluster[:,0], cluster[:,1], cmap=cmap, edgecolors='k', s=125)
 plt.show()
