fe72a61d624ed10ec7c135b174935162d9261e0a
Lukkelele <encryptedluke@protonmail.com>
2022-09-15 10:04:46 +0200

    minor cleanup

--- game_engine/engine.py
+++ game_engine/engine.py
@@ -8,9 +8,15 @@ import time
 import mesh as _mesh
 
 WIDTH, HEIGHT = 500, 500
+BLACK, WHITE = (0,0,0), (255,255,255)
+YELLOW = (255,255,0)
+GREEN = (0,255,0)
+RED = (255,0,0)
+
 
 
 class Engine:
+
 
     def __init__(self, width=500, height=500, title=""):
         self.pg = pygame.init()
@@ -22,6 +28,8 @@ class Engine:
             [0,1,0]
         ])
         self.mesh = _mesh.Mesh()
+        self.WHITE = (255,255,255) # TODO: Add color class / enums
+        self.BLACK = (0,0,0)
 
     def clearScreen(self):
         self.window.clear()
@@ -31,6 +39,12 @@ class Engine:
         pygame.quit()
         exit()
 
+    def drawTriangle(self, vertices, color=WHITE):
+        "Three vertices"
+        vec1, vec2, vec3 = vertices[0], vertices[1], vertices[2]
+        pygame.draw.line(self.window.screen, self.WHITE, vec1, vec2)
+        pygame.draw.line(self.window.screen, self.WHITE, vec1, vec3)
+        pygame.draw.line(self.window.screen, self.WHITE, vec2, vec3)
 
 e = Engine()
 e.window.screen.fill((0,0,0)) # blackscreen
@@ -68,9 +82,7 @@ while True:
         norm_x = np.subtract(line1[1] * line2[2], line1[2] * line2[1])
         norm_y = np.subtract(line1[2] * line2[0], line1[0] * line2[2])
         norm_z = np.subtract(line1[0] * line2[1], line1[1] * line2[0])
-        rotation2d_x, rotation2d_y, rotation2d_z = 0, 0, 0
 
-        #print(f"Norm vals:\n{norm_x}\n{norm_y}\n{norm_z}\n")
         for vert in tri:
             vert = np.array(vert)   # FIXME
             rotation2d_z = np.dot(e.mesh.rotation_z(angle), vert.reshape(3,1))
@@ -79,40 +91,26 @@ while True:
             projection2d = np.dot(e.proj_mat, rotation2d_x)
             x = int(projection2d[0][0] * scale + circle_pos[0]) # circle_pos -> OFFSET
             y = int(projection2d[1][0] * scale + circle_pos[1]) #               offset
-            #print(rotation2d_x);print()
-            if norm_x * rotation2d_x[0] + norm_y * rotation2d_x[1] + norm_z * rotation2d_x[2] > 0:
-                pygame.draw.circle(e.window.screen, (255,0,100), (x,y), 3)
+            #if norm_x * rotation2d_x[0] + norm_y * rotation2d_x[1] + norm_z * rotation2d_x[2] > 0:
+            #pygame.draw.circle(e.window.screen, (255,0,100), (x,y), 3)
             proj_points.append([x,y])
-        #print(proj_points)
         l1 = np.array(proj_points[2]) - np.array(proj_points[0])
         l2 = np.array(proj_points[1]) - np.array(proj_points[0])
         norm_d = np.cross(l1, l2)
         if norm_d > 0:
-            pygame.draw.line(e.window.screen, (255,255,255), proj_points[0], proj_points[1])
-            pygame.draw.line(e.window.screen, (255,255,255), proj_points[0], proj_points[2])
-            pygame.draw.line(e.window.screen, (255,255,255), proj_points[2], proj_points[1])
+            e.drawTriangle(proj_points, e.window.screen)
+            #pygame.draw.line(e.window.screen, (255,255,255), proj_points[0], proj_points[1])
+            #pygame.draw.line(e.window.screen, (255,255,255), proj_points[0], proj_points[2])
+            #pygame.draw.line(e.window.screen, (255,255,255), proj_points[2], proj_points[1])
             #pygame.draw.line(e.window.screen, (255,255,255), l2[0], l2[1])
 
         projected_points.append(proj_points)
     
-    for projected_point in projected_points:
-        # DRAW LINES
-        p1 = np.array(projected_point[0][:2])
-        p2 = np.array(projected_point[1][:2])
-        p3 = np.array(projected_point[2][:2])
-        l1 = p2 - p1
-        l2 = p3 - p1
-        angle+=0.001
-        #pygame.draw.line(e.window.screen, (255,255,255), p1, p2)
-        #pygame.draw.line(e.window.screen, (255,255,255), p2, p3)
-        #pygame.draw.line(e.window.screen, (255,255,255), p1, p3)
-
+    angle+=0.02
 
     # Update screen
     e.fpsClock.tick(30)
     e.window.update()
-
-
 
 
 
